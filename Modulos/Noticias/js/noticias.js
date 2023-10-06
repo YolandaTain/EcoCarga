abadc8ba36511c646c81699b61db57aa2f7fea89
ronnietoon <ronnietoon23@hotmail.com>
2023-10-06 10:22:22 +0200

    [Noticias] CSS y JS Actualizado

--- Modulos/Noticias/js/noticias.js
+++ Modulos/Noticias/js/noticias.js
@@ -38,31 +38,49 @@ document.addEventListener("DOMContentLoaded", function() {
     const imagenes = document.querySelectorAll(".imagen-noticia");
     const descripciones = document.querySelectorAll(".descripcion-noticia");
     const cuadroEmergente = document.getElementById("cuadro-emergente");
+    const tituloEmergente = document.getElementById("titulo-noticia-popup");
     const contenidoEmergente = document.getElementById("descripcion-noticia-popup");
     const cerrar = document.getElementById("cerrar");
-  
+
     imagenes.forEach((imagen, index) => {
-      imagen.addEventListener("click", () => {
-        contenidoEmergente.textContent = descripciones[index].textContent;
-  
-        // Obtener el tamaño y posición del carrusel
-        const carrusel = document.getElementById("carrusel-noticias");
-        const carruselAncho = carrusel.clientWidth;
-        const carruselAlto = carrusel.clientHeight;
-        const carruselPosicion = carrusel.getBoundingClientRect();
-  
-        // Establecer el ancho y alto del cuadro emergente
-        cuadroEmergente.style.width = "880px";
-        cuadroEmergente.style.height = "100px";
-  
-        // Centrar el cuadro emergente dentro del carrusel
-        cuadroEmergente.style.left = `${carruselPosicion.left}px`;
-        cuadroEmergente.style.top = `${carruselPosicion.top}px`;
-  
-        cuadroEmergente.style.display = "block";
-      });
+        imagen.addEventListener("click", () => {
+            const tituloNoticia = document.querySelectorAll(".titulo-noticia")[index];
+            contenidoEmergente.textContent = descripciones[index].textContent;
+
+            // Establece el contenido del título en el cuadro emergente
+            tituloEmergente.textContent = tituloNoticia.textContent;
+            contenidoEmergente.textContent = descripciones[index].textContent;
+
+                // Agregar la clase "cambio-color" al título para cambiar el color
+                tituloNoticia.classList.add("cambio-color");
+
+                // Restaurar el color original de otros títulos
+                const otrosTitulos = document.querySelectorAll(".titulo-noticia.cambio-color");
+                otrosTitulos.forEach((otroTitulo) => {
+                    if (otroTitulo !== tituloNoticia) {
+                        otroTitulo.classList.remove("cambio-color");
+                    }
+                });
+
+            // Obtener el tamaño y posición del carrusel
+            const carrusel = document.getElementById("carrusel-noticias");
+            const carruselAncho = carrusel.clientWidth;
+            const carruselAlto = carrusel.clientHeight;
+            const carruselPosicion = carrusel.getBoundingClientRect();
+
+            // Establecer el ancho y alto del cuadro emergente
+            cuadroEmergente.style.width = "880px";
+            cuadroEmergente.style.height = "200px";
+
+            // Centrar el cuadro emergente dentro del carrusel
+            cuadroEmergente.style.left = `${carruselPosicion.left}px`;
+            cuadroEmergente.style.top = `${carruselPosicion.top}px`;
+
+            cuadroEmergente.style.display = "block";
+        });
     });
-  });
+});
+
   
   
   
