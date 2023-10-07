f8f97b0e381dc61161d17d83a0873e9d641b7ea9
JorgeMarimon <jorgemarimon88@gmail.com>
2023-10-07 12:07:27 +0200

    [Jorge] - Intento de redireccionamiento boton final itinerario3 a home. No conseguido. Mantengo código incluido para revisar con María

--- Modulos/Itinerario/js/itinerario.js
+++ Modulos/Itinerario/js/itinerario.js
@@ -4,3 +4,8 @@ function volverHome() {
     window.open("/Modulos/Home/home.html", "_self");
 }
 
+function procesoFinalizado(){
+    alert("Itinerario enviado, disfrute!")
+    window.location.href = "/MI-PROYECTO/Modulos/Home/home.html";
+
+}
