29fd80ede054be96841e474cdea14fcb08c636bc
YolandaTain <yolandataingut@gmail.com>
2023-10-20 10:06:48 +0200

    [Yolanda] - animacion nav cambiada js

--- Modulos/Nav/js/nav.js
+++ Modulos/Nav/js/nav.js
@@ -8,13 +8,13 @@ function desplegar(){
     if (menu.style.display === "block") {
         menu.style.display = "none";
         menu.classList.remove("scale-up-hor-right");
-        menu.classList.add("scale-up-hor-right");
+        menu.classList.add("scale-up-hor-left");
       
        
 
     } else {
         menu.style.display = "block";
-        menu.classList.remove("scale-up-hor-right");
+        menu.classList.remove("scale-up-hor-left");
         menu.classList.add("scale-up-hor-right");
     }
 }
