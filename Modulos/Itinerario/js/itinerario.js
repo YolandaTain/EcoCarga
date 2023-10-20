3c05084cc707ef48733db8a1b592b5b150b6f8ad
JorgeMarimon <jorgemarimon88@gmail.com>
2023-10-20 10:48:59 +0200

    [Jorge]-Se modifica el efecto de desplazamiento del menú

--- Modulos/Itinerario/js/itinerario.js
+++ Modulos/Itinerario/js/itinerario.js
@@ -27,15 +27,15 @@ function desplegar() {
 
     if (menu.style.display === "block") {
         menu.style.display = "none";
-        menu.classList.remove("slide-in-blurred-top");
-        menu.classList.add("slide-in-blurred-top2");
+        menu.classList.remove("scale-up-hor-right");
+        menu.classList.add("scale-up-hor-left");
 
 
 
     } else {
         menu.style.display = "block";
-        menu.classList.remove("slide-in-blurred-top2");
-        menu.classList.add("slide-in-blurred-top");
+        menu.classList.remove("scale-up-hor-left");
+        menu.classList.add("scale-up-hor-right");
     }
 }
 
