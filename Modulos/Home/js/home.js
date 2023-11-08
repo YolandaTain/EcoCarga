7398774c5092e3256f72262de98ecf2b0c05c259
YolandaTain <yolandataingut@gmail.com>
2023-11-08 20:15:54 +0100

    [Yolanda] - prueba modales resultados

--- Modulos/Home/js/home.js
+++ Modulos/Home/js/home.js
@@ -17,24 +17,73 @@ function getData() {
     document.getElementById("resultadosBusqueda").innerHTML = ""
     var resultados = document.getElementById("resultadosBusqueda");
 
+    // Limpia los resultados anteriores
+    resultados.innerHTML = "";
+
     dataset.forEach(element => {
         if (resultadoBusqueda == element.codigoPostal) {
             var div = document.createElement("div");
             div.textContent = " Dirección: " + element.direccion + ", " + element.numeroDireccion
                 + " | Ciudad: " + element.ciudad + " | Código Postal: " + element.codigoPostal + " | Tipo Carga: " + element.tipoCarga;
-            
+
             // Añadir una clase al elemento div
             div.classList.add("resultado");
             div.classList.add("col-10");
             div.classList.add("col-md-8");
             div.classList.add("col-lg-6")
-            
-            resultados.appendChild(div);
-        }
 
+            resultados.appendChild(div);
+
+            // Crea un botón para mostrar el modal
+            var infoButton = document.createElement("button");
+            infoButton.textContent = "+Info";
+            infoButton.classList.add("btn", "btn-dark");
+
+            // Agrega un manejador de eventos para mostrar el modal cuando se hace clic en el botón
+            infoButton.addEventListener("click", function () {
+                showInfoModal(element);
+            });
+
+            // Agrega el botón al div de resultados
+            div.appendChild(infoButton);
+
+            // Agrega el div al contenedor de resultados
+            resultados.appendChild(div);
+
+        } else {
+            // Si no se encuentra el código postal, muestra un mensaje de error en un modal
+            var modal = document.getElementById("ModalCenterF");
+            var modalTitleF = modal.querySelector(".modal-titleF");
+            var modalContentF = modal.querySelector(".modal-bodyF");
+            modalTitleF.textContent = "Código Postal no encontrado";
+            modalContentF.innerHTML = "El código postal que ingresaste no se encuentra en la base de datos.";
+            // Muestra el modal
+            modal.classList.add("show");
+            modal.style.display = "block";
+            document.body.classList.add("modal-open");
+            document.body.style.paddingRight = "15px"; // Ajusta el desplazamiento del cuerpo si es necesario
+        }
     });
 }
 
+function showInfoModal(element) {
+    var modal = document.getElementById("exampleModalCenter");
+    var modalTitle = modal.querySelector(".modal-title");
+    var modalContent = modal.querySelector(".modal-body");
+  
+    modalTitle.textContent = "Información del Código Postal: " + element.codigoPostal;
+  
+    modalContent.innerHTML = "Dirección: " + element.direccion + ", " + element.numeroDireccion
+      + " | Ciudad: " + element.ciudad + " | Código Postal: " + element.codigoPostal + " | Tipo Carga: " + element.tipoCarga;
+  
+    // Muestra el modal
+    modal.classList.add("show");
+    modal.style.display = "block";
+    document.body.classList.add("modal-open");
+    document.body.style.paddingRight = "15px"; // Ajusta el desplazamiento del cuerpo si es necesario
+  }
+
+  
 
 function resultado() {
     var menu = document.getElementById("destacados");
