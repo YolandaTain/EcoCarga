d19b0ced079dae2283913bd76029a067a1599c54
YolandaTain <yolandataingut@gmail.com>
2023-11-14 20:42:28 +0100

    [Yolanda] - modales

--- Modulos/Home/js/home.js
+++ Modulos/Home/js/home.js
@@ -20,10 +20,13 @@ function getData() {
     // Limpia los resultados anteriores
     resultados.innerHTML = "";
 
+    // Variable para realizar un seguimiento de si se encontró el código postal
+    var codigoPostalEncontrado = false;
+
     dataset.forEach(element => {
         if (resultadoBusqueda == element.codigoPostal) {
             var div = document.createElement("div");
-            div.textContent = " Dirección: " + element.direccion + ", " + element.numeroDireccion
+            div.textContent = element.direccion + ", " + element.numeroDireccion
                 + " | Ciudad: " + element.ciudad + " | Código Postal: " + element.codigoPostal + " | Tipo Carga: " + element.tipoCarga;
 
             // Añadir una clase al elemento div
@@ -36,6 +39,10 @@ function getData() {
 
             // Crea un botón para mostrar el modal
             var infoButton = document.createElement("button");
+            infoButton.style.padding = '1rem';
+            infoButton.style.margin = '1rem';
+            infoButton.style.width = '10rem';
+            infoButton.style.fontSize = '2rem';
             infoButton.textContent = "+Info";
             infoButton.classList.add("btn", "btn-dark");
 
@@ -50,40 +57,61 @@ function getData() {
             // Agrega el div al contenedor de resultados
             resultados.appendChild(div);
 
-        } else {
-            // Si no se encuentra el código postal, muestra un mensaje de error en un modal
-            var modal = document.getElementById("ModalCenterF");
-            var modalTitleF = modal.querySelector(".modal-titleF");
-            var modalContentF = modal.querySelector(".modal-bodyF");
-            modalTitleF.textContent = "Código Postal no encontrado";
-            modalContentF.innerHTML = "El código postal que ingresaste no se encuentra en la base de datos.";
-            // Muestra el modal
-            modal.classList.add("show");
-            modal.style.display = "block";
-            document.body.classList.add("modal-open");
-            document.body.style.paddingRight = "15px"; // Ajusta el desplazamiento del cuerpo si es necesario
+            // Cambia la variable a true porque encontraste un código postal
+            codigoPostalEncontrado = true;
         }
     });
+
+    // Después del bucle, verifica si se encontró el código postal o no
+    if (!codigoPostalEncontrado) {
+        // Si no se encuentra el código postal, muestra un mensaje de error en un modal
+        var modal = document.getElementById("ModalCenterF");
+        var modalTitleF = modal.querySelector(".modal-titleF");
+        var modalContentF = modal.querySelector(".modal-bodyF");
+        modalTitleF.textContent = "Código Postal no encontrado";
+        modalContentF.innerHTML = "El código postal que ingresaste no se encuentra en la base de datos.";
+        // Muestra el modal
+        modal.classList.add("show");
+        modal.style.display = "block";
+        document.body.classList.add("modal-open");
+        document.body.style.paddingRight = "15px"; // Ajusta el desplazamiento del cuerpo si es necesario
+    }
 }
 
 function showInfoModal(element) {
     var modal = document.getElementById("exampleModalCenter");
     var modalTitle = modal.querySelector(".modal-title");
     var modalContent = modal.querySelector(".modal-body");
-  
+
     modalTitle.textContent = "Información del Código Postal: " + element.codigoPostal;
-  
-    modalContent.innerHTML = "Dirección: " + element.direccion + ", " + element.numeroDireccion
-      + " | Ciudad: " + element.ciudad + " | Código Postal: " + element.codigoPostal + " | Tipo Carga: " + element.tipoCarga;
-  
-    // Muestra el modal
+
+    modalContent.innerHTML =
+        "Dirección: " + element.direccion + ", " + element.numeroDireccion +
+        " | Ciudad: " + element.ciudad +
+        " | Código Postal: " + element.codigoPostal +
+        " | Tipo Carga: " + element.tipoCarga;
+
+    // Muestra el modal utilizando Bootstrap clases
     modal.classList.add("show");
     modal.style.display = "block";
     document.body.classList.add("modal-open");
-    document.body.style.paddingRight = "15px"; // Ajusta el desplazamiento del cuerpo si es necesario
-  }
+    initMap()
 
-  
+}
+
+function initMap() {
+    var myLatLng = { lat: 36.7201600, lng: -4.4203400 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      zoom: 12
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Calle Alhambra, Málaga'
+    });
+  }
 
 function resultado() {
     var menu = document.getElementById("destacados");
